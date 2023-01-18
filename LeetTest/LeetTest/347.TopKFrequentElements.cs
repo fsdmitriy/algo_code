476cee332165aefa2e09f7f7e7e5114f39bf930f
Dmitriy <dmitriy.fokasii@gmail.com>
2023-01-18 13:31:22 +0200

    solved using bucket sorting algoritm

---
+++ LeetTest/LeetTest/347.TopKFrequentElements.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+TopKFrequent(new int[] { 1, 1, 1, 2, 2, 3 }, 2);
+TopKFrequent(new int[] { 1 }, 1);
+
+// Bucket sort algoritm
+int[] TopKFrequent(int[] nums, int k)
+{
+	var dict = new Dictionary<int, int>();
+
+	for (int i = 0; i < nums.Length; i++)
+	{
+		dict.TryGetValue(nums[i], out int count);
+
+		dict[nums[i]] = ++count;
+	}
+
+	var bucket = new List<int>[nums.Length + 1];
+
+	foreach (var keyValuePair in dict)
+	{
+		if (bucket[keyValuePair.Value] == null)
+			bucket[keyValuePair.Value] = new List<int>();
+
+		bucket[keyValuePair.Value].Add(keyValuePair.Key);
+	}
+
+	var result = new int[k];
+	int indx = 0;
+
+	for (int i = bucket.Length - 1; i > 0; i--)
+	{
+		if (bucket[i]!=null && bucket[i].Count > 0)
+		{
+			for (int j = 0; j < bucket[i].Count; j++)
+			{
+				result[indx++] = bucket[i][j];
+
+				if (indx == k)
+					return result;
+			}
+		}
+	}
+
+	return result;
+}
