967fa001cd403449d7a2e88ec889058f2900fc65
Dmitriy <dmitriy.fokasii@gmail.com>
2023-01-20 18:31:38 +0200

    solved longest consecutive

---
+++ LeetTest/LeetTest/128.LongestConsecutiveSequence.cs
@@ -0,0 +1,24 @@
+﻿
+LongestConsecutive(new int[] { 100, 4, 200, 1, 3, 2 });
+
+int LongestConsecutive(int[] nums)
+{
+    // make sure that every number is unique
+    var hashSet = new HashSet<int>(nums);
+
+    int longest = 1;
+    foreach (var num in hashSet)
+    {
+        if (!hashSet.Contains(num - 1))
+        {
+            int currentSequence = 1;
+
+            while (hashSet.Contains(num + currentSequence))
+                currentSequence++;
+
+            longest = Math.Max(longest, currentSequence);
+        }
+    }
+
+    return longest;
+}
