a079f9a88237df91b29218c47830008ab567b0b9
Dmitriy <dmitriy.fokasii@gmail.com>
2023-02-02 18:54:06 +0200

    linked list solved

---
+++ LeetTest/LeetTest/206.ReverseLinkedList.cs
@@ -0,0 +1,30 @@
+﻿
+ReverseList(new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))))));
+
+ListNode ReverseList(ListNode head)
+{
+    ListNode prev = null;
+    ListNode curr = head;
+
+    while (curr != null)
+    {
+        var listNode = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = listNode;
+    }
+
+    return prev;
+}
+
+
+class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
---
+++ LeetTest/LeetTest/234.PalindromeLinkedList.cs
@@ -0,0 +1,64 @@
+﻿
+IsPalindrome(new ListNode(1, new ListNode(2, new ListNode(2, new ListNode(1)))));
+IsPalindrome(new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4)))));
+
+bool IsPalindrome(ListNode head)
+{
+    if (head == null) return false;
+
+    ListNode middlePoint = GetFirstHalfNode(head);
+    ListNode secongReversed = GetReversedListNode(middlePoint.next);
+    
+    while(head!=null && secongReversed !=null)
+    {
+        if (head.val != secongReversed.val) return false;
+
+        head = head.next;
+        secongReversed= secongReversed.next;  
+    }
+
+    //Time - O(N) Space - O(1)
+    return true;
+}
+
+ListNode GetFirstHalfNode(ListNode head)
+{
+    ListNode fast = head;
+    ListNode slow = head;
+
+    while (fast.next != null && fast.next.next != null)
+    {
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    return slow;
+}
+
+ListNode GetReversedListNode(ListNode head)
+{
+    ListNode prev = null;
+    ListNode curr = head;
+    while (curr != null)
+    {
+        var tmp = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = tmp;
+    }
+
+    return prev;
+}
+
+
+
+
+class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
