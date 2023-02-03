f7e1d984fcb4652a22a0b95817936e4cc353d94f
Dmitriy <dmitriy.fokasii@gmail.com>
2023-02-03 21:21:23 +0200

    two pointers

---
+++ LeetTest/LeetTest/11.ContainerWithMostWater.cs
@@ -0,0 +1,21 @@
+﻿
+MaxArea(new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 });
+MaxArea(new int[] { 3, 2, 4, 2, 3, 2, 1, 2, 1, 4 });
+MaxArea(new int[] { 1, 1 });
+
+int MaxArea(int[] height)
+{
+    int p1 = 0, p2 = height.Length - 1, result = 0;
+
+    while (p1 < p2)
+    {
+        result = Math.Max((Math.Min(height[p1], height[p2]) * (p2 - p1)), result);
+
+        if (height[p1] > height[p2])
+            p2--;
+        else
+            p1++;
+    }
+
+    return result;
+}
