ab17741a0be40a0eb46e623900fcb669e5d13aca
Dmitriy <dmitriy.fokasii@gmail.com>
2023-02-06 18:30:34 +0200

    sliding window

---
+++ LeetTest/LeetTest/121.BestTimetoBuyandSellStock.cs
@@ -0,0 +1,16 @@
+﻿
+MaxProfit(new int[] { 7, 1, 5, 3, 6, 4 });
+MaxProfit(new int[] { 7, 6, 4, 3, 1 });
+
+int MaxProfit(int[] prices)
+{
+    int minPrice = prices[0], result = 0;
+
+    for (int i = 1; i < prices.Length; i++)
+    {
+        minPrice = Math.Min(minPrice, prices[i]);
+        result = Math.Max(result, prices[i] - minPrice);
+    }
+
+    return result;
+}
---
+++ LeetTest/LeetTest/3.LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,31 @@
+﻿LengthOfLongestSubstring(" ");
+LengthOfLongestSubstring("aab");
+LengthOfLongestSubstring("abcabcbb");
+LengthOfLongestSubstring("bbbbb");
+LengthOfLongestSubstring("pwwkew");
+
+int LengthOfLongestSubstring(string s)
+{
+    if (s.Length == 1)
+        return s.Length;
+
+    var set = new HashSet<char>();
+
+    int p1 = 0; int p2 = 0, result = 0;
+    while (p2 < s.Length)
+    {
+        if (set.Add(s[p2]))
+        {
+            p2++;
+        }
+        else
+        {     
+            set.Remove(s[p1]);
+            p1++;
+        }
+
+        result = Math.Max(result, p2-p1);
+    }
+
+    return result;
+}
