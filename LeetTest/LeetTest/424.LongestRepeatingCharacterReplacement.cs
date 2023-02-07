a07530b6cd3459bc5e5e5bea804e6ef98e3d4515
Dmitriy <dmitriy.fokasii@gmail.com>
2023-02-07 15:33:53 +0200

    sliding window

---
+++ LeetTest/LeetTest/424.LongestRepeatingCharacterReplacement.cs
@@ -0,0 +1,31 @@
+﻿
+CharacterReplacement("ABAB", 2);
+CharacterReplacement("AABABBA", 1);
+
+int CharacterReplacement(string s, int k)
+{
+    var dict = new Dictionary<char, int>();
+    int result = 0, leftPoint = 0, rigthPoint = 0, currMaxFriquent = 0;
+
+    while (rigthPoint < s.Length)
+    {
+        dict.TryGetValue(s[rigthPoint], out int val);
+
+        dict[s[rigthPoint]] = ++val;
+
+        currMaxFriquent = Math.Max(currMaxFriquent, dict[s[rigthPoint]]);
+
+        if (rigthPoint - leftPoint + 1 - currMaxFriquent > k)
+        {
+            dict[s[leftPoint]]--;
+            currMaxFriquent--;
+            leftPoint++;
+        }
+
+        result = Math.Max(result, rigthPoint - leftPoint + 1);
+
+        rigthPoint++;
+    }
+
+    return result;
+}
