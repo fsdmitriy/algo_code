7fc7ed1700bf4c7ebb8685606de9c8b807724d3d
Dmitriy <dmitriy.fokasii@gmail.com>
2023-02-16 18:11:49 +0200

    binary search

---
+++ LeetTest/LeetTest/153.FindMinimuminRotatedSortedArray.cs
@@ -0,0 +1,26 @@
+﻿
+FindMin(new int[] { 3, 1, 2 });
+FindMin(new int[] { 4, 5, 6, 7, 0, 1, 2 });
+FindMin(new int[] { 3, 4, 5, 1, 2 });
+FindMin(new int[] { 11, 13, 15, 17 });
+
+int FindMin(int[] nums)
+{
+    int left = 0;
+    int right = nums.Length - 1;
+
+    while (left <= right)
+    {
+        if (nums[left] <= nums[right])
+            return nums[left];
+
+        int mid = (left + right) / 2;
+
+        if (nums[mid] >= nums[right])
+            left = mid + 1;
+        else
+            right = mid;
+    }
+
+    return 0;
+}
