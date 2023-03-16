dfac0c1804a88bed30acc161bd81007f9a68fb53
Dmitriy <dmitriy.fokasii@gmail.com>
2023-03-16 18:36:48 +0200

    binary tree

---
+++ LeetTest/LeetTest/235.LowestCommonAncestorofaBinarySearchTree.cs
@@ -0,0 +1,24 @@
+﻿using LeetTest.Models;
+
+LowestCommonAncestor(new TreeNode(2, new TreeNode(1)), new TreeNode(2), new TreeNode(1));
+
+LowestCommonAncestor(new TreeNode(6, new TreeNode(2, new TreeNode(0), new TreeNode(4, new TreeNode(3), new TreeNode(5))), new TreeNode(8, new TreeNode(7),
+    new TreeNode(9))), new TreeNode(2), new TreeNode(8));
+
+LowestCommonAncestor(new TreeNode(6, new TreeNode(2, new TreeNode(0), new TreeNode(4, new TreeNode(3), new TreeNode(5))), new TreeNode(8, new TreeNode(7),
+    new TreeNode(9))), new TreeNode(2), new TreeNode(4));
+
+TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+{
+    if(p.val<root.val && q.val<root.val)
+    { 
+        return LowestCommonAncestor(root.left, p, q);
+    }
+
+    if(p.val>root.val && q.val > root.val)
+    {
+        return LowestCommonAncestor(root.right, p, q);
+    }
+
+    return root;
+}
---
+++ LeetTest/LeetTest/572.SubtreeofAnotherTree.cs
@@ -0,0 +1,30 @@
+﻿using LeetTest.Models;
+
+IsSubtree(new TreeNode(3, new TreeNode(4, new TreeNode(1), new TreeNode(2, new TreeNode(0))), new TreeNode(5)), new TreeNode(4, new TreeNode(1), new TreeNode(2)));
+IsSubtree(new TreeNode(3, new TreeNode(4, new TreeNode(1), new TreeNode(2)), new TreeNode(5)), new TreeNode(4, new TreeNode(1), new TreeNode(2)));
+IsSubtree(new TreeNode(1, new TreeNode(1)), new TreeNode(1));
+
+bool IsSubtree(TreeNode root, TreeNode subRoot)
+{
+    if (root == null)
+        return false;
+
+    if (IsSameTree(root, subRoot))
+        return true;
+
+    return IsSubtree(root.left, subRoot) || IsSubtree(root.right, subRoot);
+}
+
+bool IsSameTree(TreeNode root, TreeNode subRoot)
+{
+    if (root == null && subRoot == null)
+        return true;
+    else if (root != null && subRoot == null)
+        return false;
+    else if (root == null && subRoot != null)
+        return false;
+    else if (root.val != subRoot.val)
+        return false;
+
+    return IsSameTree(root.left, subRoot.left) && IsSameTree(root.right, subRoot.right);
+}
