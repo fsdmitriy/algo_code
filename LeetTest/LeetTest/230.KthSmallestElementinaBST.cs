4c30d3f36c8d5a32cb62b1e9ec639b9408678d71
Dmitriy <dmitriy.fokasii@gmail.com>
2023-03-29 19:26:57 +0300

    Trees algorithm

---
+++ LeetTest/LeetTest/230.KthSmallestElementinaBST.cs
@@ -0,0 +1,22 @@
+﻿using LeetTest.Models;
+
+KthSmallest(new TreeNode(3, new TreeNode(1, new TreeNode(2)), new TreeNode(4)), 1);
+
+int KthSmallest(TreeNode root, int k)
+{
+    var stack = new Stack<TreeNode>();
+
+    while (true)
+    {
+        while (root != null)
+        {
+            stack.Push(root);
+            root = root.left;
+        }
+
+        root = stack.Pop();
+        if (--k == 0) return root.val;
+
+        root = root.right;
+    }
+}
---
+++ LeetTest/LeetTest/98.ValidateBinarySearchTree.cs
@@ -0,0 +1,19 @@
+﻿using LeetTest.Models;
+
+IsValidBST(new TreeNode(5, new TreeNode(4), new TreeNode(6, new TreeNode(3), new TreeNode(7))));
+// DFS algorithm
+bool IsValidBST(TreeNode root)
+{
+    return ValidateBST(root, null, null);
+}
+
+bool ValidateBST(TreeNode root, TreeNode left, TreeNode right)
+{
+    if (root == null)
+        return true;
+
+    if ((right != null && root.val >= right.val) || (left != null && root.val <= left.val))
+        return false;
+
+    return ValidateBST(root.left, left, root) && ValidateBST(root.right, root, right);
+}
